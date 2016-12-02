514ee68b718d46a34afa4c2fd6de365b98357b6a
Alfredo Sauceda <freddysauceda@gmail.com>
2016-12-02 00:02:22 -0800

    Water is working as a blue square.

--- Water.cpp
+++ Water.cpp
@@ -50,6 +50,8 @@ void Water::draw(glm::mat4 C){
     //glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
     glDrawArrays(GL_TRIANGLES, 0, 6);
     glBindVertexArray(0);
+    
+    
 }
 
 void Water::update(){
